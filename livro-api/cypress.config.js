36b2e31b104f1ea6862befdad1ca372675c1e6e9
Junior-AlvesNewDev <onoffjunior@gmail.com>
2025-08-24 11:32:52 -0300

    segunda parte automatização de todo o processo de testes de ponta a ponta com o Cypress

---
+++ livro-api/cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+const { configurePlugin } = require('cypress-mongodb');
+
+module.exports = defineConfig({
+  env: {
+    mongodb: {
+      uri: 'mongodb+srv://dba:junior123@livroapi.2cossat.mongodb.net/?retryWrites=true&w=majority&appName=LivroApi',
+      database: 'test',
+      collection: 'livros'
+    }
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      configurePlugin(on);
+    },
+  },
+});
